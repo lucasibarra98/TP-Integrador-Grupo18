a5d9ae357eff451eac42134cd9b14a3e7cf69d24
lucasibarra98 <ibarralucas.1998@gmail.com>
2024-11-11 17:37:19 -0300

    Eliminar archivo 01_CreacionSP.sql

--- 01_CreacionSP.sql
+++
@@ -1,7 +0,0 @@
-/*
-	Entrega 3
-
-	Genere store procedures para manejar la inserción, modificado, borrado (si corresponde,
-	también debe decidir si determinadas entidades solo admitirán borrado lógico) de cada tabla.
-	Los nombres de los store procedures NO deben comenzar con “SP”.
-*/
