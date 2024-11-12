074659988cae0258dea637d7e15733d7c6df5201
lucasibarra98 <ibarralucas.1998@gmail.com>
2024-11-12 15:16:02 -0300

    Agregacion de SP de asignacion de roles (falta crear los usuarios)

--- CreacionRoles.sql
+++ CreacionRoles.sql
@@ -22,10 +22,39 @@ BEGIN
 END;
 GO
 
+-- Ejecutamos el SP para que se creen los roles
 EXEC CrearRolesDesdeTablaCargo;
 GO
 
+-- Verificamos si los roles fueron creados
 SELECT name
 FROM sys.database_principals
 WHERE type = 'R';
 GO
+
+--Falta crear usuarios para que se puedan asignar los roles
+/*
+CREATE OR ALTER PROCEDURE AsignarRolAEmpleado
+AS
+BEGIN
+    DECLARE @sql NVARCHAR(MAX);
+
+    -- Inicializamos la variable @sql
+    SET @sql = '';
+
+    -- Construir dinámicamente el SQL para asignar roles a los usuarios
+    SELECT @sql = @sql + 
+        'EXEC sp_addrolemember ''' + c.nombre + ''', ''' + e.nombre + ''';'
+    FROM negocio.Empleado e
+    INNER JOIN negocio.Cargo c ON e.idCargo = c.id  -- Hacemos el JOIN entre Empleado y Cargo
+    WHERE c.nombre IS NOT NULL AND e.nombre IS NOT NULL;
+
+    -- Ejecutar el SQL dinámico
+    EXEC sp_executesql @sql;
+
+    PRINT 'Roles asignados a los Empleados desde su tabla correspondiente.';
+END;
+
+EXEC AsignarRolAEmpleado;
+*/
+
